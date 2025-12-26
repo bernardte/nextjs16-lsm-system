cc105b21532ecaf2af48cba00d9731ba22acf994
TYH <yuhang028@gmail.com>
2025-12-27 02:32:59 +0800

    add prisma and better-auth configuration also include of login page UI

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { GithubIcon } from "lucide-react";
 
 const LoginPage = () => {
@@ -10,11 +12,24 @@ const LoginPage = () => {
         <CardDescription>Login with your GitHub or Email Account</CardDescription>
       </CardHeader>
 
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
         <Button variant={"outline"} className="w-full">
           <GithubIcon className="size-4"/>
             Sign in with GitHub
         </Button>
+
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">Or continue with</span>
+        </div>
+
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" placeholder="your@example.com"/>
+          </div>
+
+          <Button>Continue with Email</Button>
+        </div>
       </CardContent>
     </Card>
   )
